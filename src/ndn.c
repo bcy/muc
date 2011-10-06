373c4cdbb5a4b65b9dd15b6e99a771e2acb71092
Chaoyi Bian <bcy@pku.edu.cn>
2011-10-06 00:27:41 -0700

    create_presence_interest implementation

--- src/ndn.c
+++ src/ndn.c
@@ -104,6 +104,32 @@ ndn_run(gpointer data)
   return res;
 }
 
+static int /* for qsort */
+namecompare(const void *a, const void *b)
+{
+    const struct ccn_charbuf *aa = *(const struct ccn_charbuf **)a;
+    const struct ccn_charbuf *bb = *(const struct ccn_charbuf **)b;
+    int ans = ccn_compare_names(aa->buf, aa->length, bb->buf, bb->length);
+    if (ans == 0)
+        fprint(stderr, "wassat? %d\n", __LINE__);
+    return (ans);
+}
+
+static void
+copy_from_list(struct ccn_charbuf **res, GQueue *list)
+{
+  GQueue duplica = g_queue_copy(list);
+  char *element;
+  int idx = 0;
+  while ((element = g_queue_pop_head(duplica)) != NULL)
+  {
+    struct ccn_charbuf *temp = ccn_charbuf_create();
+    ccn_name_init(temp);
+    ccn_name_append_str(temp, element);
+    res[idx++] = temp;
+  }
+}
+
 static int
 create_presence_interest(char *roomid, GQueue *exclusion_list)
 {
@@ -133,6 +159,66 @@ create_presence_interest(char *roomid, GQueue *exclusion_list)
   }
   
   excl = calloc(sizeof(struct ccn_charbuf) * g_queue_get_length(exclusion_list));
+  copy_from_list(excl, exclusion_list);
+  qsort(excl, g_queue_get_length(exclusion_list), sizeof(struct ccn_charbuf), &namecompare);
+  
+  int begin = 0;
+  bool excludeLow = false;
+  bool excludeHigh = true;
+  while (begin < g_queue_get_length(exclusion_list))
+  {
+    if (begin != 0)
+      excludeLow = true;
+    
+    struct ccn_charbuf *templ = ccn_charbuf_create();
+    ccn_charbuf_append_tt(templ, CCN_DTAG_Interest, CCN_DTAG); // <Interest>
+    ccn_charbuf_append_tt(templ, CCN_DTAG_Name, CCN_DTAG); // <Name>
+    ccn_charbuf_append_closer(templ); // </Name> 
+    ccn_charbuf_append_tt(templ, CCN_DTAG_Exclude, CCN_DTAG); // <Exclude>
+    
+    if (excludeLow)
+      append_bf_all(templ);
+    
+    for (; begin < g_queue_get_length(exclusion_list); begin++)
+    {
+      struct ccn_charbuf *comp = excl[begin];
+      if (comp->length < 4)
+	abort();
+      
+      // we are being conservative here
+      if (interest->length + templ->length + comp->length > 1350)
+      {
+	break;
+      }
+      
+      ccn_charbuf_append(templ, comp->buf + 1, comp->length - 2);
+    }
+    
+    if (begin == g_queue_get_length(exclusion_list))
+      excludeHigh = false;
+
+    if (excludeHigh)
+      append_bf_all(templ);
+    
+    ccn_charbuf_append_closer(templ); // </Exclude>
+    ccn_charbuf_append_closer(templ); // </Interest> 
+    int res = ccn_express_interest(ccn, interest, nthread->in_content_presence, templ);
+    if (res < 0)
+    {
+      log_error(NAME, "ccn_express_interest failed!");
+      return 1;
+    }
+    
+    ccn_charbuf_destroy(&templ);
+  }
+  
+  ccn_charbuf_destroy(&interest);
+  for (int i = 0; i < g_queue_get_length(exclusion_list); i++)
+    ccn_charbuf_destroy(&excl[i]); 
+
+  if (excl != NULL)
+    delete []excl;
+  
   return 0;
 }
 
