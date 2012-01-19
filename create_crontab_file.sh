fb55c1c741bf8c35f5dbc8bc0d2c56f65fa4534f
Chaoyi Bian <tlyzbcy@gmail.com>
2012-01-19 11:17:45 -0800

    remove create_crontab_file.sh

--- create_crontab_file.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "@reboot /usr/local/bin/ccndstart" > crontab_file
-echo "@reboot /usr/local/bin/jabberd -h localhost" >> crontab_file
-echo "@reboot cd `pwd` && src/mu-conference -c muc.xml" >> crontab_file
-
